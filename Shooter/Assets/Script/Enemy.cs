e4d337f6a5c756a5f6816404638e64dc52735943
Che Shian Hung <hungc@bgsu.edu>
2019-07-27 01:00:08 -0400

    enemy blinks when hit

---
+++ Shooter/Assets/Script/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+	public float blinkingTimer = 0.0f;
+	public float blinkingDuration = 0.1f;
+	public bool startBlinking = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public virtual void Update()
+    {
+		if(startBlinking == true)
+		{ 
+			StartBlinking();
+		}
+    }
+
+	private void StartBlinking()
+	{
+		blinkingTimer += Time.deltaTime;
+		if(blinkingTimer >= blinkingDuration)
+		{
+			blinkingTimer = 0.0f;
+			if (this.gameObject.GetComponent<SpriteRenderer> ().enabled == true) {
+				this.gameObject.GetComponent<SpriteRenderer> ().enabled = false;  //make changes
+			} else {
+				this.gameObject.GetComponent<SpriteRenderer> ().enabled = true;   //make changes
+				startBlinking = false;
+			}
+		}
+	}
+}
--- Shooter/Assets/Script/Enemy_Triangle.cs
+++ Shooter/Assets/Script/Enemy_Triangle.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_Triangle : MonoBehaviour
+public class Enemy_Triangle : Enemy
 {
     public Transform target;
+	public float health;
 
     // Start is called before the first frame update
     void Start()
@@ -13,16 +14,19 @@ public class Enemy_Triangle : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public override void Update()
     {
         Vector3 diff = (target.position - transform.position).normalized;
         float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+
+		base.Update();
     }
 
     void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.transform.tag == "PlayerAttack")
-            Debug.Log("hit");
+		if (col.transform.tag == "PlayerAttack") {
+			startBlinking = true;
+		}
     }
 }
