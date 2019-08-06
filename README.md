bac5f91d1f35d64ac52c7c95e607710deb6752ab
Che Shian Hung <hungc@bgsu.edu>
2019-08-05 23:51:26 -0400

    triangle wave now spawns multiple enemy, items updated

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Unity-Shooter
+
+TODO
+triangle level
+wave end
+wave start
+
+Item
+1. Hard to Miss: double the bullet size
+2. Break: gain 1 heart
+3. Trade Off: increase the shoot speed but decrease the damage 
+4. Deformity: add 1 more bullet at each shoot, but now the shooter shoots diagnolly
+5. Successful Diet: move slightly faster
+6. Angel Bless: when the shooter is hit at the first time in a wave, it causes no damage
--- Shooter/Assets/Script/SmallWave_Triangle.cs
+++ Shooter/Assets/Script/SmallWave_Triangle.cs
@@ -5,15 +5,51 @@ using UnityEngine;
 public class SmallWave_Triangle : SmallWave
 {
 	public GameObject enemy;
-	public int total = 20;
+	public GameObject shooter;
+
+	private int totalEnemy = 30;
+	private int currentEnemyCount = 0;
+	private float minTime = 0.5f;
+	private float maxTime = 1.5f;
+	private float intervalTime;
+	private float timer = 0.0f;
+	private bool allEnemySpawn = false;
 
     void Start()
     {
-        
+		intervalTime = Random.Range(minTime, maxTime);
     }
 
     void Update()
     {
-        
+		if (!allEnemySpawn) {
+			timer += Time.deltaTime;
+			if (timer >= intervalTime) {
+				currentEnemyCount++;
+				intervalTime = Random.Range(minTime, maxTime);
+				timer = 0f;
+				SpawnEnemy();
+				if (currentEnemyCount >= totalEnemy)
+					allEnemySpawn = true;
+			}
+				
+		}
     }
+
+	void SpawnEnemy()
+	{
+		float angle = Random.Range(0, 180);
+		float rad = angle * Mathf.Deg2Rad;
+		float tan = Mathf.Tan(rad);
+		Vector2 direction = new Vector2(1 / tan, 1);
+
+		RaycastHit2D[] hits = Physics2D.RaycastAll(new Vector3(0, 0, 0), direction, 15);
+		foreach (RaycastHit2D hit in hits) {
+			if (hit.collider.gameObject.tag == "Respawn") {
+				enemy.transform.position = new Vector3(hit.point.x, hit.point.y, -1);
+				enemy.GetComponent<Enemy_Triangle>().target = shooter;
+				Instantiate(enemy);
+			}
+		}
+	}
 }
